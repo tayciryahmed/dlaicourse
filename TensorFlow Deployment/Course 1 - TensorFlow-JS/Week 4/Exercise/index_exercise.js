b214472ae84331fe1e840026064bb134273200df
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-06-07 16:34:50 +0200

    Add files via upload

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
@@ -23,22 +23,24 @@ async function train() {
   // to use as many hidden layers and neurons as you like.  
   // HINT: Take a look at the Rock-Paper-Scissors example. We also suggest
   // using ReLu activation functions where applicable.
+
   model = tf.sequential({
     layers: [
-        
-      // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: 'relu'}),
+      tf.layers.dense({ units: 5, activation: 'softmax'})
     ]
   });
+
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001);
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -75,7 +77,10 @@ function handleButton(elem){
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
             
-        // YOUR CODE HERE
+                case "4":
+			lizardSamples++;
+			document.getElementById("lizardsamples").innerText = "Lizard samples:" + spockSamples;
+			break;
 		
             
 	}
@@ -112,7 +117,9 @@ async function predict() {
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
             
-        // YOUR CODE HERE 
+                case 4:
+			predictionText = "I see lizard";
+			break;
 	
             
 	}
